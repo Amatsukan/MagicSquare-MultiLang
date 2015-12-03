20a1f2f8975a7312f05077fa06646c26c953b792
amatsukan <luiz.cfj@grad.ufsc.br>
2015-12-03 15:44:42 -0200

    okay

---
+++ magicSquare.pas
@@ -0,0 +1,79 @@
+program MagicSquare;
+uses sysutils;
+
+var
+    actual_val, x, y, L, LIMIT: integer;
+    square : array of array of integer;
+begin
+    L := StrToInt(ParamStr(1));
+    LIMIT := L*L;
+
+    if (L MOD 2)=1 then
+        writeln('1. A matriz sempre terá os lados ímpares.')
+        setlength(square,L,L)
+    else
+        writeln('FALHA: violação da regra 1, \n\t1. A matriz sempre terá os lados ímpares.')
+end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
